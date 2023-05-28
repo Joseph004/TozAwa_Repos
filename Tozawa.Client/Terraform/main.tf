0831d8010b28dd2f57f92af65d78433788aceb56
Joseph Luhandu <josephluhandu@yahoo.com>
2023-05-28 20:30:28 +0200

    corect syntax code

--- Tozawa.Client/Terraform/main.tf
+++ Tozawa.Client/Terraform/main.tf
@@ -20,11 +20,11 @@ resource "azurerm_resource_group" "tozawa_nonprod" {
 }
 
 resource "aws_instance" "container_nonprod" {
-  ami           = "ami-0989fb15ce71ba39e"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.deployer_nonprod.key_name
+  ami                    = "ami-0989fb15ce71ba39e"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.deployer_nonprod.key_name
   vpc_security_group_ids = [aws_security_group.nonprod-group.id]
-  iam_instance_profile = aws_iam_instance_profile.ec2-nonprod-profile.name
+  iam_instance_profile   = aws_iam_instance_profile.ec2-nonprod-profile.name
   connection {
     type        = "ssh"
     host        = self.public_ip
@@ -43,16 +43,18 @@ resource "aws_iam_instance_profile" "ec2-nonprod-profile" {
 }
 
 resource "aws_security_group" "nonprod-group" {
-  egress = {
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = ""
-    from_port        = 0
-    iov6_cidr_blocks = []
-    protocol         = "-1"
-    security_groups  = []
-    self             = false
-    to_port          = 0
-  }
+  egress = [
+    {
+      cidr_blocks      = ["0.0.0.0/0"]
+      description      = ""
+      from_port        = 0
+      iov6_cidr_blocks = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
   ingress = [
     {
       cidr_blocks      = ["0.0.0.0/0"]
@@ -83,6 +85,6 @@ resource "aws_key_pair" "deployer_nonprod" {
 }
 
 output "instance_public_ip" {
-  value = aws_instance.container_nonprod.public_ip
+  value     = aws_instance.container_nonprod.public_ip
   sensitive = true
 }
