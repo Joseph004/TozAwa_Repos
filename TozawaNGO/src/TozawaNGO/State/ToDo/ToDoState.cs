fa5e4a231f5a76bae24edadb7c11dc3048eb162d
Joseph Luhandu <josephluhandu@yahoo.com>
2024-02-21 22:10:17 +0100

    state fd

--- TozawaNGO/src/TozawaNGO/State/ToDo/ToDoState.cs
+++ TozawaNGO/src/TozawaNGO/State/ToDo/ToDoState.cs
@@ -9,6 +9,6 @@ public record ToDoState
 {
     public string NewItem { get; init; }
     public bool IsLoading { get; init; }
-    public TodoKeyedCollection Todos { get; init; }
+    public TodoKeyedCollection Todos { get; init; } = new();
     public Orleans.Streams.StreamSubscriptionHandle<TodoNotification> Subscription { get; init; }
 }
